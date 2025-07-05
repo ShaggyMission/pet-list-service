583ac04b552c91f1ef9cefa426e9577b54559039
Lis Pacheco <lmpacheco@uce.edu.ec>
2025-07-05 13:39:56 -0500

    fix: refactored MongoDB connection and fixed export error

--- config/db.js
+++ config/db.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 
-const uri = 'mongodb://admin:Lis12345@54.156.27.209:27017/admin';
+const connectDB = async () => {
+  const uri = 'mongodb://admin:Lis12345@54.156.27.209:27017/admin';
 
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.error('MongoDB connection error:', err));
+  try {
+    await mongoose.connect(uri);
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error('MongoDB connection error:', err);
+  }
+};
 
 module.exports = connectDB;
