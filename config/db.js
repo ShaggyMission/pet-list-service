fc5c050dc72cf4a3f810d201a27f7ab86cbf01a8
Lis Pacheco <lmpacheco@uce.edu.ec>
2025-07-05 13:28:16 -0500

    fix: corrected MongoDB connection error

--- config/db.js
+++ config/db.js
@@ -9,5 +9,4 @@ mongoose.connect(uri, {
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.error('MongoDB connection error:', err));
 
-module.exports = mongoose;
-
+module.exports = connectDB;
