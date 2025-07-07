a6b30a6198194e14a49fd4b7a5c04f92e08d3185
Shaggy Mission <rescuefriendsuio@gmail.com>
2025-07-06 23:41:20 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const YAML = require('yamljs');
 const app = express();
 
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: 'http://3.208.223.30:3000',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type'],
 };
